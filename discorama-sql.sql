a4fb03e8ea9f1be44ebf5d19448ca3358ae7cac0
Suelen Rodrigues <124621480+suelennfr@users.noreply.github.com>
2023-02-14 16:07:04 -0300

    Update discorama-sql.sql

--- discorama-sql.sql
+++ discorama-sql.sql
@@ -1,7 +1,17 @@
+*/ 
+PROJETO ANALISTA DE DADOS
+CLIENTE FICTÍCIO: DISCORAMA
+PROGRAMA LIGHTHOUSE - INDICIUM
+*\
+
+-- Verificando a relação entre o dataset de aluguel com o de pagamento
+
 SELECT rental.rental_id, rental.customer_id, rental.rental_date, rental.return_date, payment.amount, payment.payment_date
 FROM Discorama.dbo.rental AS rental
 INNER JOIN Discorama.dbo.payment AS payment
 ON rental.rental_id = payment.rental_id;
+
+-- Integrando datasets para analisar dados de pagamento e aluguel por país
 
 SELECT DISTINCT rental.rental_id, rental.rental_date, customer.customer_id, address.city_id, city.country_id, country.country, payment.amount
 FROM Discorama.dbo.customer AS customer
